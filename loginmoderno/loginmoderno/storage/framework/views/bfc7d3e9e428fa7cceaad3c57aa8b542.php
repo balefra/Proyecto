e28b5090e68130f4b1434ce32b9a78f593bbedde
Macarobu <maria.c39robledo@gmail.com>
2024-10-21 14:56:59 -0500

    proyecto

---
+++ loginmoderno/loginmoderno/storage/framework/views/bfc7d3e9e428fa7cceaad3c57aa8b542.php
@@ -0,0 +1,100 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+<link rel="stylesheet" href="<?php echo e(asset('assets/estilos.css')); ?>">
+
+
+
+    </head>
+
+    <body>
+        <section class="h-100 gradient-form" style="background-color: #eee;">
+            <div class="container py-5 h-100">
+              <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-xl-10">
+                  <div class="card rounded-3 text-black">
+                    <div class="row g-0">
+                      <div class="col-lg-6">
+                        <div class="card-body p-md-5 mx-md-4">
+          
+                          <div class="text-center">
+                            <img src="<?php echo e(asset('assets/images/iberov2.png')); ?>"
+                              style="width: 100px;" alt="logo">
+                              <img src="<?php echo e(asset('assets/images/uNIVERSIDA TRES CULTURAS.png')); ?>"
+                              style="width: 185px;" alt="logo">
+                            <h4 class="mt-1 mb-5 pb-1">Virtual Research Assistant</h4>
+                          </div>
+          
+                          <form action="<?php echo e(route('login')); ?>" method="post">
+                            <?php echo csrf_field(); ?>
+                            <p>Iniciar sesión </p>
+          
+                            <div data-mdb-input-init class="form-outline mb-4">
+                              <input type="email" name="email" id="form2Example11" class="form-control"
+                                placeholder="Ingresa el correo" />
+                              <label class="form-label" for="form2Example11">Ingresar el correo electrónico </label>
+                            </div>
+          
+                            <div data-mdb-input-init class="form-outline mb-4">
+                              <input type="password" name="password" id="form2Example22" class="form-control" />
+                              <label class="form-label" for="form2Example22">Ingresar la contraseña</label>
+                            </div>
+          
+                            <div class="text-center pt-1 mb-5 pb-1">
+                              <button data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="submit">Iniciar</button>
+                              <a class="text-muted" href="#!">¿Olvido la contraseña? </a>
+                            </div>
+          
+                            <div class="d-flex align-items-center justify-content-center pb-4">
+                              <p class="mb-0 me-2">¿No tiene cuenta?</p>
+                              <a href="<?php echo e(route('register')); ?>" class="btn btn-outline-danger">Crear una nueva cuenta</a>
+                            </div>
+          
+                          </form>
+          
+                        </div>
+                      </div>
+                      <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+                        <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                          <h4 class="mb-4">Virtual Research Assistant</h4>
+                          <p class="small mb-0">descripción .</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+
+
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
+<?php /**PATH C:\Proyectos\loginmoderno\resources\views/auth/login.blade.php ENDPATH**/ ?>
---
+++ loginmoderno/loginmoderno/storage/framework/views/e07961d92b1b8ba43fbe863526e2d963.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>B. componente Realidad</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <link rel="stylesheet" href="<?php echo e(asset('assets/estiloss.css')); ?>">
+</head>
+
+<body>
+<div class="cabecera">
+        <h1>Modelado Investigación</h1>
+    </div>
+    <div class="container">
+        <img src="<?php echo e(asset('assets/images/iberov2.png')); ?>"  class="iberov">
+        <img src="<?php echo e(asset('assets/images/uNIVERSIDA TRES CULTURAS.png')); ?>" class="utc">
+    </div>
+
+    
+    <form action="#" method="post" class="form-container">
+    <h3>Componente Realidad</h3>
+        
+    
+      <label for="fenomeno">Fenómeno</label>
+      <textarea  rows="7" id="fenomeno"  placeholder="Situación de una realidad que se presenta de manera arbitraria a los parámetros esperados y que genera la inquietud o necesidad de interpretar/comprender, describir el fenómeno." required></textarea>
+      
+        <label for="necesidad">Necesidad</label>
+      <textarea rows="7" id="fenomeno"  placeholder="Situación de una realidad que se constituye en un imperativo a cumplir nolmalmente asociado con el cumplimiento de una determinada imposicion politica o legal, describir necesidad." required></textarea>
+
+      <label for="problematica">Problemática</label>
+      <textarea rows="7" id="problematica" placeholder="Situación de una realidad que afecta el bienestar de un individuo  o una organización y que genera la necesidad imperiosa de resolver /suplir." required></textarea>
+
+      <label for="experiencia">Experiencia</label>
+      <textarea  rows="7" id="experiencia" placeholder="Acción consciente o deliberada, normalmente no documentada a través de la cual se  ha interpretado un fenómeno o resuelto una problemática  y que genera la necesidad de sistematizar / compilar." required></textarea>
+
+ 
+       
+        <center>
+    <a href="<?php echo e(url('/welcome')); ?>" >Atrás</a>
+    <a href="<?php echo e(url('/Realidad')); ?>" >Siguiente</a>
+    </center>
+    </form>
+
+    
+    <body>
+      
+  
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
+<?php /**PATH C:\Proyectos\loginmoderno\resources\views/Realidad.blade.php ENDPATH**/ ?>
