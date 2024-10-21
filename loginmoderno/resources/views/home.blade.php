346ae16b851b1c296f77158db5893d4df0cc5d00
balefra <franedubale23@gmail.com>
2024-10-21 17:10:29 -0500

    cerrar sesion

--- loginmoderno/resources/views/home.blade.php
+++ loginmoderno/resources/views/home.blade.php
@@ -1,9 +1,9 @@
 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-    <a class="dropdown-item" href="{{ route('logout') }}"
+    <button class="dropdown-item" href="{{ route('logout') }}"
        onclick="event.preventDefault();
                      document.getElementById('logout-form').submit();">
         {{ __('cerrar sesión') }}
-    </a>
+    </button>
 
     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
         @csrf
